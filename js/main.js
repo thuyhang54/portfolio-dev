7d8550677c8bb00fee149f6d2072971d890098a2
T.H <thuyhang8563@gmail.com>
2024-10-29 17:16:32 +0700

    fix menu

--- js/main.js
+++ js/main.js
@@ -5,21 +5,24 @@ const overlay = document.querySelector("#overlay");
 const body = document.body;
 const menuItems = menu.querySelectorAll("li");
 function toggleMenu() {
-    menu.classList.toggle('navbar__open');
-    menuButton.classList.toggle('navbar__icons--active');
-    overlay.classList.toggle('show');
-    body.classList.toggle('no-scroll', menu.classList.contains('navbar__open'));
+        menu.classList.toggle('navbar__open');
+        menuButton.classList.toggle('navbar__icons--active'); 
+        overlay.classList.toggle('show');
+        body.classList.toggle('no-scroll', menu.classList.contains('navbar__open'));
 }
 menuButton.addEventListener('click', toggleMenu);
 overlay.addEventListener('click', toggleMenu);
-menuItems.forEach(item=>{
-    item.addEventListener('click', toggleMenu); 
-})
-// Resize the menu
-window.addEventListener("resize",()=>{
-    if (window.innerWidth > 768 && menu.classList.contains('navbar__open')) {
+menuItems.forEach(item=>{item.addEventListener('click', ()=>{
+    if (window.innerWidth <= 768) {
         toggleMenu();
     }
+}   
+)});
+// Close the menu if the window is resized to be wider than 768px
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 768 && menu.classList.contains('navbar__open')) {
+        toggleMenu();
+    }   
 });
 
 // header fixed
